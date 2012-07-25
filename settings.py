d9862e9d198765ce73d22a7edce7bd7aa33cb838
David Smith <dcs206@ecs.soton.ac.uk>
2012-07-25 11:02:00 +0100

    Added redmine details to database list.

--- settings.py
+++ settings.py
@@ -18,7 +18,15 @@ DATABASES = {
         'PASSWORD': 'cicada',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                        # Set to empty string for default. Not used with sqlite3.
-    }
+    },
+    'redmine': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'redmine',                      # Or path to database file if using sqlite3.
+        'USER': 'redmine',                      # Not used with sqlite3.
+        'PASSWORD': 'cicada-redmine',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                        # Set to empty string for default. Not used with sqlite3.
+    },
 }
 
 # Local time zone for this installation.
